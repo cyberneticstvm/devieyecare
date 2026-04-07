a291e0dc3dd54782a9760e0ccd812d16e9e9d6fa
Vijoy Sasidharan <mail@cybernetics.me>
2026-04-07 14:50:35 +0530

    small updates

--- resources/views/admin/pdf/envelope.blade.php
+++ resources/views/admin/pdf/envelope.blade.php
@@ -111,7 +111,7 @@
             <!-- QR Code -->
             <div class="qr-code">
                 <img src="data:image/png;base64, {!! $qrcode !!}"
-                    alt="QR Code" style="height: 50px; width: 50px; margin-right: 30px margin-top:50px">
+                    alt="QR Code" style="height: 50px; width: 50px; margin-right: 30px; margin-top: 50px">
                 <div style="font-size: 10px; margin-top: 5px;">{{ $stock->material->code }}</div>
             </div>
         </div>
