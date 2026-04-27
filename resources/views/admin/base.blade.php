cd7aa988e9f27e91df9e464817b1b9d042dc9315
Cybernetics <mail@cybernetics.me>
2026-04-27 16:13:43 +0530

    small updates

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -26,7 +26,7 @@
 
 </head>
 
-<body data-thunderal="{{ (web_app_name() == 'devi') ? 'theme-indigo' : 'theme-orange' }}" class="">
+<body data-thunderal="{{ (web_app_name() == 'devi') ? 'theme-indigo' : 'theme-orange' }}" class="{{ web_app_name() }}">
     <!-- Start:: Header -->
     <header class="header sticky-top" id="header">
         <div class="container-fluid px-2 px-md-4">
