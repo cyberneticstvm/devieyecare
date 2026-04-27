ac1c5bd629e9e5f52d0c1fa643c7668e3d675450
Cybernetics <mail@cybernetics.me>
2026-04-27 16:11:15 +0530

    small updates

--- resources/views/admin/drishti/customer/edit.blade.php
+++ resources/views/admin/drishti/customer/edit.blade.php
@@ -12,42 +12,42 @@
                         <div class="col-lg-12">
                             {{ html()->form('POST')->route('drishti.customer.update', encrypt($customer->id))->class('')->open() }}
                             <div class="row g-3">
-                                <div class="control-group col-md-6">
+                                <div class="control-group col-md-3">
                                     <label class="form-label req">Customer Name </label>
                                     {{ html()->text('name', old('name') ?? $customer->name)->class('form-control')->placeholder('Customer Name') }}
                                     @error('name')
                                     <small class="text-danger">{{ $errors->first('name') }}</small>
                                     @enderror
                                 </div>
-                                <div class="control-group col-md-6">
+                                <div class="control-group col-md-2">
                                     <label class="form-label req">Contact Number </label>
                                     {{ html()->text('mobile', old('mobile') ?? $customer->mobile)->class('form-control')->maxlength(10)->placeholder('Contact Number') }}
                                     @error('mobile')
                                     <small class="text-danger">{{ $errors->first('mobile') }}</small>
                                     @enderror
                                 </div>
-                                <div class="control-group col-md-12">
+                                <div class="control-group col-md-5">
                                     <label class="form-label">Address </label>
                                     {{ html()->text('address', old('address') ?? $customer->address)->class('form-control')->placeholder('Address') }}
                                     @error('address')
                                     <small class="text-danger">{{ $errors->first('address') }}</small>
                                     @enderror
                                 </div>
-                                <div class="control-group col-md-4">
+                                <div class="control-group col-md-2">
                                     <label class="form-label">GST Number </label>
                                     {{ html()->text('gst', old('gst') ?? $customer->gst)->class('form-control')->placeholder('GST Number') }}
                                     @error('gst')
                                     <small class="text-danger">{{ $errors->first('gst') }}</small>
                                     @enderror
                                 </div>
-                                <div class="control-group col-md-4">
+                                <div class="control-group col-md-2">
                                     <label class="form-label">Opening Balance </label>
                                     {{ html()->text('opening_balance', old('opening_balance') ?? $customer->opening_balance)->class('form-control')->placeholder('Opening Balance') }}
                                     @error('opening_balance')
                                     <small class="text-danger">{{ $errors->first('opening_balance') }}</small>
                                     @enderror
                                 </div>
-                                <div class="control-group col-md-4">
+                                <div class="control-group col-md-2">
                                     <label class="form-label">Credit Limit </label>
                                     {{ html()->text('credit_limit', old('credit_limit') ?? $customer->credit_limit)->class('form-control')->placeholder('Credit Limit') }}
                                     @error('credit_limit')
