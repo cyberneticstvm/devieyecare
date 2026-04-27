9cb691cd6ef36ac9c85cc4a9ffc69f97688488d0
Cybernetics <mail@cybernetics.me>
2026-04-27 12:51:26 +0530

    small updates

--- config/myconfig.php
+++ config/myconfig.php
@@ -3,4 +3,6 @@
 return [
     'gcs_url_public' => 'https://storage.googleapis.com',
     'gcs_url_private' => 'https://storage.cloud.google.com',
+
+    'web_app_name' => env('WEB_APP_NAME', 'devi'),
 ];
