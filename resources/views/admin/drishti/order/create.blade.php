d0348718f71e3ae76d19ffb3deb51d820b694368
Cybernetics <mail@cybernetics.me>
2026-04-28 10:33:32 +0530

    small updates

--- resources/views/admin/drishti/order/create.blade.php
+++ resources/views/admin/drishti/order/create.blade.php
@@ -83,13 +83,13 @@
                                                 <label class="form-label req">Qty</label>
                                                 {{ html()->number('qty', '', '1', '', '1')->class('form-control')->placeholder('0') }}
                                             </div>
-                                            <!--<div class="control-group col-md-4">
-                                                <label class="form-label req">Pur.Price</label>
-                                                {{ html()->number('purchase_price', '', '1', '', 'any')->class('form-control')->placeholder('0.00') }}
-                                            </div>-->
                                             <div class="control-group col-md-4">
                                                 <label class="form-label req">Sell.Price</label>
                                                 {{ html()->number('selling_price', '', '1', '', 'any')->class('form-control')->placeholder('0.00') }}
+                                            </div>
+                                            <div class="control-group col-md-4">
+                                                <label class="form-label req">Pur.Price</label>
+                                                {{ html()->hidden('purchase_price', 0, 0, 0, 'any')->class('form-control')->placeholder('0.00') }}
                                             </div>
                                         </div>
                                         <div class="raw text-end mt-3">
